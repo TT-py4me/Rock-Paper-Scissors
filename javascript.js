9b57b6731d98b2690997e42d56fd3bb6f50bd24d
TT-py4me <51959897+TT-py4me@users.noreply.github.com>
2023-04-01 16:14:54 -0400

    Create function to return game winner

--- javascript.js
+++ javascript.js
@@ -8,16 +8,73 @@ const answer = choices[answer_index]
 return(answer)
 };
 
-console.log(getComputerChoice()) //test function
-console.log(getComputerChoice())
-console.log(getComputerChoice())
-console.log(getComputerChoice()) 
-console.log(getComputerChoice())
-console.log(getComputerChoice())
-console.log(getComputerChoice()) 
-console.log(getComputerChoice())
-console.log(getComputerChoice())
 
+//play single round
+const oneRoundRPS = function(computerSelection, playerSelection) {
 
+const winner=""; 
+const roundstatus="";
+
+if(computerSelection === "rock") { //checking memory 
+  
+  switch(playerSelection) {
+    case  "rock":
+        roundstatus = "tie"
+        break;
+    case "paper":
+        winner = "player"
+        break;
+    case "scissors":
+        winner = "computer"
+        break;
+  }
+} else if(computerSelection === "paper") {
+  
+  switch(playerSelection) {
+    case  "rock":
+        winner= "computer"
+        break;
+    case "paper":
+        roundstatus = "tie"
+        break;
+    case "scissors":
+        winner = "player"
+        break;
+  }
+} else if(computerSelection === "scissors") {
+
+  switch(playerSelection) {
+    case  "rock":
+        winner = "player"
+        break;
+    case "paper":
+        winner = "computer"
+        break;
+    case "scissors":
+        roundstatus = "tie"
+        break;
+  }
+}
+
+//return results
+if(roundstatus != "tie"){
+    return(winner)
+}else {
+    return(roundstatus)
+}
+ 
+};
+
+/*
+
+const player = input //dowhile loop for input add later
+
+const computer = getComputerChoice()
+//call
+oneRoundRPS(computer,player) //argue function with computer and player choices
+
+//later, use do while to replay a tie, in game function
+
+*/
 
 
